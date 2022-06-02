b5e6e2e6920e452debf753092d62b8e318b4d77b
Omphile <omphilekopeka@gmail.com>
2022-06-02 16:04:08 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
 #include<stdio.h>
-int main(void) {puts("\"Programming is like building a multilingual puzzle"); return (0);}
+int main(void)
+/**
+ * the function main is the initial entry point for the programme
+ * the return value: 0
+ */
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
