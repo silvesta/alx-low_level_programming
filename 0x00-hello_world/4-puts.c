d85074f7897b969096f4e35981deaacf75efb15f
Omphile <omphilekopeka@gmail.com>
2022-06-02 15:50:28 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -c main | puts("\"Programming is like building a multilingual puzzle"); return 0;
+int main(void) {puts("\"Programming is like building a multilingual puzzle"); return (0);}
