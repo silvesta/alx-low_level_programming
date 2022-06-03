4c67c1738947a634c5c49bac992f2eb0c88d2765
Omphile <omphilekopeka@gmail.com>
2022-06-02 18:06:14 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ *  main - entry point for program
+ *  Return: 1
+ */
+
+int main(void)
+{	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	retur (1);
+}
