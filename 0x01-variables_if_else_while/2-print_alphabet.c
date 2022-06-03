59f4250eafd6526fc2a48884ad394dab12502836
Omphile <omphilekopeka@gmail.com>
2022-06-03 13:49:19 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program
+ * Description: Print all letters of the alphabet in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{	char letterrs = 'a';
+
+	while (letters <= 'z')
+	{	putchar(letters);
+		letters = letters + 1;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
