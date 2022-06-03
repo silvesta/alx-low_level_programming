e8d40032117a65d3ebff66da072c6a28433b5f82
Omphile <omphilekopeka@gmail.com>
2022-06-03 15:59:16 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ * Description: Print all single digit base ten numbers
+ * Return: 0
+ */
+
+int main(void)
+{	int base_Ten;
+
+	for (base_Ten = 0; base_Ten < 10; x++)
+	{	printf("%d", base_Ten);
+	}
+
+	return (0);
+
+}
