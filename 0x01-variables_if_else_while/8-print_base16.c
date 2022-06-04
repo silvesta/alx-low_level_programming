d82fe7db59c9b5544a7857d378cedf861f5838b5
Omphile <omphilekopeka@gmail.com>
2022-06-03 17:11:45 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,18 +2,25 @@
 
 /**
  * main - Entry point for the program
- * Description: Prints base 16 numbers.
+ * Description: Prints all base 16 numbers.
  * Return: 0
  */
 
 int main(void)
 {
-	int base_16 = 0;
+	char numbers = '0';
+	char letters = 'a';
 
-	while (base_16 < 16)
+	while (numbers <= 9)
 	{
-		putchar(base_16);
-		base_16 = base_16 + 1;
+		putchar(numbers);
+		numbers++;
+	}
+
+	while (letters <= 'f')
+	{
+		putchar(letters);
+		letters++;
 	}
 
 	putchar('\n');
