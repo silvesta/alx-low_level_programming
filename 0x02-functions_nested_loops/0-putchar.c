0cccdfadc6543d443561e91d39a2f8f8087691db
Omphile <omphilekopeka@gmail.com>
2022-06-07 09:46:42 -0700

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - Entry point for the program
+ * Description: Program prints the string _putchar
+ * Retun: 0
+ */
+
+int main(void)
+{
+	char a = "_putchar"
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * main - Entry point for the program
+ * Description: Program prints all letters of the alphabet in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char letters = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(letters);
+		letters++;
+	}
+
+	_putchar('\n');
+}
