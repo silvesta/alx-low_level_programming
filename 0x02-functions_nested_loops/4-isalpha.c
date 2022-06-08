2f05084c7f7e1f338a53b512edad7e250b70d089
Omphile <omphilekopeka@gmail.com>
2022-06-08 14:50:37 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,7 @@
 
 int _isalpha(int c)
 {
-	int check = (c >= 97 && c <= 122) || (c >= 65 && c <= 90) ? : 0;
+	int check = (c >= 97 && c <= 122) || (c >= 65 && c <= 90) ? 1 : 0;
 
 	return (check);
 }
