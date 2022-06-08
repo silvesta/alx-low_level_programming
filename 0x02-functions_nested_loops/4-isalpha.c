504029bd85f078b72816e1874e34da43e63d6db4
Omphile <omphilekopeka@gmail.com>
2022-06-08 14:48:03 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Function prototype
+ * Description: Determine if a character is a letter or not
+ * @c: Test ascii character
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	int check = (c >= 97 && c <= 122) || (c >= 65 && c <= 90) ? : 0;
+
+	return (check);
+}
